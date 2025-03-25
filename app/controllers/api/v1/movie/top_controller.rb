9041261d1ab7e0164dd348d9b73fa7ab8f3b5fe5
Sebastian McKee <sebmcke@gmail.com>
2025-03-25 16:45:30 -0500

    Create method for top 20 movies

--- app/controllers/api/v1/movie/top_controller.rb
+++ app/controllers/api/v1/movie/top_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::Movie::TopController < ApplicationController
     def index
-        render json: "test"
+        conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+            faraday.headers["Authorization"] = Rails.application.credentials.tmdb[:read_token]
+        end
+
+        response = conn.get("/3/movie/top_rated")
+        json = JSON.parse(response.body, symbolize_names: true)
+
+        render json: MovieSerializer.format_movie_list(json[:results])
     end
 end
