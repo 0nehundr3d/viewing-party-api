073b011d682ac9990accbbd5fc5d8388c9d64a7d
Sebastian McKee <sebmcke@gmail.com>
2025-03-26 15:34:40 -0500

    Add validations testing to viewing parties

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,4 +1,8 @@
 class ViewingParty < ApplicationRecord
+    validates :name, presence: true
+    validates :start_time, presence: true
+    validates :end_time, presence: true
+    validates :movie_id, presence: true
     has_many :viewing_party_users
     has_many :users, through: :viewing_party_users
 end
