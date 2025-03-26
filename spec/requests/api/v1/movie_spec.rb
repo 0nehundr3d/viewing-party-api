d296886bbf38fb665858caa887910201f5beca6a
Sebastian McKee <sebmcke@gmail.com>
2025-03-26 14:23:57 -0500

    Add sad path for empty search to movie search endpoint

--- spec/requests/api/v1/movie_spec.rb
+++ spec/requests/api/v1/movie_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "Movie Api", type: :request do
     describe "Movie Search" do
-        it "can use query params to return 20 relevant movie results" do
+        it "can use query params to return 20 relevant movie results", :vcr do
             get "/api/v1/movie?search=Matrix"
             json = JSON.parse(response.body, symbolize_names: true)
 
@@ -12,6 +12,15 @@ describe "Movie Api", type: :request do
             expect(json[:data].first[:type]).to eq("movie")
             expect(json[:data].first[:attributes][:title]).to be_a(String)
             expect(json[:data].first[:attributes][:vote_average]).to be_a(Float)
+        end
+
+        it "returns a 400 error when not adding a search", :vcr do
+            get "/api/v1/movie?search"
+            json = JSON.parse(response.body, symbolize_names: true)
+            
+            expect(response).to have_http_status(:bad_request)
+            expect(json[:message]).to eq("A search is required for the movie endpoint")
+            expect(json[:status]).to eq(400)
         end
     end
 end
