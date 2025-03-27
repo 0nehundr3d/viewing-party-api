fb7fb1cd1aac8448572217fc155e00d6f5c2de2f
Sebastian McKee <sebmcke@gmail.com>
2025-03-27 14:58:28 -0500

    Add invite_users method

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -3,12 +3,13 @@ class ViewingParty < ApplicationRecord
     validates :start_time, presence: true
     validates :end_time, presence: true
     validates :movie_id, presence: true
+    validates :movie_title, presence: true
     has_many :viewing_party_users
     has_many :users, through: :viewing_party_users
 
     def invite_users(users)
-        users.each do |user|
-            ViewingPartyUser.create!(viewing_party: self, user: user)
+        users.each do |user_id|
+            ViewingPartyUser.create!(viewing_party: self, user: User.where(id: user_id)[0])
         end
     end
 end
