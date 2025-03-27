7eec8433328670010baa8ea894e7a53b56596628
Sebastian McKee <sebmcke@gmail.com>
2025-03-27 14:58:01 -0500

    Add create method to party controller

--- app/controllers/api/v1/users/party_controller.rb
+++ app/controllers/api/v1/users/party_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::Users::PartyController < ApplicationController
     def create
-        # binding.pry
+        viewing_party = ViewingParty.create!(viewing_party_params)
+        viewing_party.invite_users(params[:invitees])
+
+        render json: ViewingPartySerializer.serialize(viewing_party), status: 201
+    end
+
+    private
+
+    def viewing_party_params
+        params.permit(:name, :start_time, :end_time, :movie_id, :movie_title)
     end
 end
