aebff58c7176f103467b0883599cb4a9bebcb8ac
Sebastian McKee <sebmcke@gmail.com>
2025-03-27 15:19:24 -0500

    Add host validation test for viewing party user

--- spec/models/viewing_party_users_spec.rb
+++ spec/models/viewing_party_users_spec.rb
@@ -1,6 +1,9 @@
 require "rails_helper"
 
 describe ViewingPartyUser, type: :model do
+    describe "validations" do
+        it { should validate_inclusion_of(:host).in_array([true,false]) }
+    end
     describe "relations" do
         it { should belong_to :user }
         it { should belong_to :viewing_party }
