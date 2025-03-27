53e91262762f3fd74fbf1c959afa8e93d6e3b9e3
Sebastian McKee <sebmcke@gmail.com>
2025-03-27 15:01:21 -0500

    Add poroify method

--- app/gateways/movie_gateway.rb
+++ app/gateways/movie_gateway.rb
@@ -1,18 +1,12 @@
 class MovieGateway
     def self.movie_search(search)
         response = conn.get("3/search/movie?query=#{search}")
-        json = JSON.parse(response.body,symbolize_names: true)
-        json[:results].map do |movie_data|
-            Movie.new(movie_data)
-        end
+        poroify(response)
     end
 
     def self.top_movies 
         response = conn.get("/3/movie/top_rated")
-        json = JSON.parse(response.body, symbolize_names: true)
-        json[:results].map do |movie_data|
-            Movie.new(movie_data)
-        end
+        poroify(response)
     end
 
     private
@@ -20,6 +14,14 @@ class MovieGateway
     def self.conn
         Faraday.new(url: "https://api.themoviedb.org") do |faraday|
             faraday.headers["Authorization"] = Rails.application.credentials.tmdb[:read_token]
+        end
+    end
+
+    def self.poroify(response)
+        response = conn.get("/3/movie/top_rated")
+        json = JSON.parse(response.body, symbolize_names: true)
+        json[:results].map do |movie_data|
+            Movie.new(movie_data)
         end
     end
 end
