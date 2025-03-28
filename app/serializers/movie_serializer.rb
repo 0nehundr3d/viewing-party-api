ac27daebfac4712039fdf467422cf88878ae2b33
Sebastian McKee <sebmcke@gmail.com>
2025-03-28 14:36:51 -0500

    Create movie details serializer

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -17,4 +17,24 @@ class MovieSerializer
         end
     }
   end
+
+  def self.serialize_details(movie)
+    {
+      data: {
+        id: movie.id.to_s,
+        type: "movie",
+        attributes: {
+          title: movie.title,
+          release_year: movie.release_year,
+          vote_average: movie.vote_average,
+          runtime: movie.runtime,
+          genres: movie.genres,
+          summary: movie.summary,
+          cast: movie.cast,
+          total_reviews: movie.total_reviews,
+          reviews: movie.reviews
+        }
+      }
+    }
+  end
 end
