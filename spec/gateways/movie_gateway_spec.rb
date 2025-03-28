6358c325b71e5ec523d4bf8f0197e8d9f0b6966f
Sebastian McKee <sebmcke@gmail.com>
2025-03-28 14:29:48 -0500

    Add test for movie fetch_movie_details method

--- spec/gateways/movie_gateway_spec.rb
+++ spec/gateways/movie_gateway_spec.rb
@@ -26,4 +26,21 @@ RSpec.describe MovieGateway do
             expect(MovieGateway.fetch_movie_runtime(278)).to eq(142)
         end
     end
+
+    describe "#fetch_movie_details" do
+        it "should return details of a movie" do
+            movie = MovieGateway.fetch_movie_details(278)
+
+            expect(movie).to be_a(Movie)
+            expect(movie.title).to eq("The Shawshank Redemption")
+            expect(movie.release_year).to eq(1994)
+            expect(movie.vote_average).to eq(8.709)
+            expect(movie.runtime).to eq("2 hours, 22 minutes")
+            expect(movie.genres).to eq(["Drama", "Crime"])
+            expect(movie.summary).to eq("Imprisoned in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.")
+            expect(movie.cast.count).to be <= 10
+            expect(movie.total_reviews).to eq(17)
+            expect(movie.reviews.count).to be <= 5
+        end
+    end
 end
