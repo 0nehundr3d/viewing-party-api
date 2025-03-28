f87ad601f230a4f12d16808ef3aa639ae613969e
Sebastian McKee <sebmcke@gmail.com>
2025-03-28 14:18:35 -0500

    Update movie poro to have all required detail

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,11 +1,42 @@
 class Movie 
     attr_reader :title,
                 :vote_average,
-                :id
+                :id,
+                :release_date,
+                :summary,
+                :cast
 
     def initialize(movie_hash)
         @title = movie_hash[:title]
+        @release_date = movie_hash[:release_date]
         @vote_average = movie_hash[:vote_average]
+        @runtime = movie_hash[:runtime]
+        @genres = movie_hash[:genres]
+        @summary = movie_hash[:overview]
+        @cast = movie_hash[:cast][0..9]
+        @reviews = movie_hash[:results]
         @id = movie_hash[:id]
+    end
+
+    def release_year 
+        @release_date[0..3].to_i
+    end
+
+    def runtime 
+        "#{@runtime/60} hours, #{@runtime%60} minutes"
+    end
+
+    def genres 
+        @genres.map do |genre|
+            genre[:name]
+        end
+    end
+
+    def reviews
+        @reviews[0..4]
+    end
+
+    def total_reviews
+        @reviews.count
     end
 end
--- spec/poros/movie_spec.rb
+++ spec/poros/movie_spec.rb
@@ -3,31 +3,22 @@ require "rails_helper"
 RSpec.describe Movie do
     describe "#initialize" do
         it "Should be able to create a movie object from a hash" do
-            movie_data = {
-                adult: false,
-                backdrop_path: "/zfbjgQE1uSd9wiPTX4VzsLi0rGG.jpg",
-                genre_ids: [
-                    18,
-                    80
-                ],
-                id: 278,
-                original_language: "en",
-                original_title: "The Shawshank Redemption",
-                overview: "Imprisoned in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.",
-                popularity: 28.6761,
-                poster_path: "/9cqNxx0GxF0bflZmeSMuL5tnGzr.jpg",
-                release_date: "1994-09-23",
-                title: "The Shawshank Redemption",
-                video: false,
-                vote_average: 8.708,
-                vote_count: 27967
-            }
+            movie_data = JSON.parse(File.read("spec/fixtures/movie/movie.json"), symbolize_names: true)
+            .merge(JSON.parse(File.read("spec/fixtures/movie/cast.json"), symbolize_names: true))
+            .merge(JSON.parse(File.read("spec/fixtures/movie/reviews.json"), symbolize_names: true))
 
             movie = Movie.new(movie_data)
 
+            expect(movie).to be_a(Movie)
             expect(movie.title).to eq("The Shawshank Redemption")
-            expect(movie.vote_average).to eq(8.708)
-            expect(movie.id).to eq(278)
+            expect(movie.release_year).to eq(1994)
+            expect(movie.vote_average).to eq(8.709)
+            expect(movie.runtime).to eq("2 hours, 22 minutes")
+            expect(movie.genres).to eq(["Drama", "Crime"])
+            expect(movie.summary).to eq("Imprisoned in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.")
+            expect(movie.cast.count).to be <= 10
+            expect(movie.total_reviews).to eq(17)
+            expect(movie.reviews.count).to be <= 5
         end
     end
 end
